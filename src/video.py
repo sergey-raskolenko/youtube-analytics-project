717b7ecb331af1331cf2f977757d29629f91124f
Sergey Raskolenko <sergey.raskolenko@gmail.com>
2023-06-23 02:05:26 +0300

    Changed arithmetic tests

--- src/video.py
+++ src/video.py
@@ -33,9 +33,10 @@ class Video:
         return self.__video_like_count
 
     def video_info(self):
-        """Возвращает список словарей с информацией о канале"""
+        """Возвращает список словарей с информацией о видео"""
         youtube = self.get_service()
-        video_object = youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails', id=self.video_id).execute()
+        video_object = youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails', id=self.video_id
+                                             ).execute()
         return video_object
 
     def print_info(self) -> None:
@@ -46,3 +47,28 @@ class Video:
     def get_service(cls):
         """Возвращает объект для работы с YouTube API"""
         return build('youtube', 'v3', developerKey=cls.api_key)
+
+class PLVideo(Video):
+
+    def __init__(self, video_id, playlist_id):
+        super().__init__(video_id)
+        self.__playlist_id = playlist_id
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}('{self.video_id}', '{self.playlist_id}')"
+
+    def __str__(self) -> str:
+        return f'{self.video_title}'
+
+    def playlist_info(self):
+        """Возвращает список словарей с информацией о плэйлисте"""
+        youtube = self.get_service()
+        playlist_object = youtube.playlistItems().list(playlistId=self.playlist_id,
+                                               part='contentDetails, snippet', maxResults=50,
+                                               ).execute()
+        return playlist_object
+
+    @property
+    def playlist_id(self):
+        return self.__playlist_id
+
